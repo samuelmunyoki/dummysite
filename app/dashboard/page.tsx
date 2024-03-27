10b8f98b619d8cecc873f8b5e83bcfcb9b5afbb9
samuelmunyoki <samuelmuthembwa@yahoo.com>
2024-03-27 15:46:42 +0300

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,187 @@
+"use client";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+export default function Home() {
+  const [passwordProtected, setPasswordProtected] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const [password, setPassword] = useState<string>("");
+  const [zipname, setZipname] = useState<string>("");
+  const [isloading, setIsloading] = useState<boolean>(false);
+  const [uploadStatus, setUploadStatus] = useState<string>(""); // To store upload status
+  async function handleDownload() {
+    try {
+      console.log(zipname);
+      const response = await fetch(
+        `http://localhost:8080/download?zipname=${zipname}`
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Failed to download zip file. Server returned ${response.status} ${response.statusText}`
+        );
+      }
+
+      // Get the response body as a Blob
+      const blob = await response.blob();
+
+      // Create a temporary anchor element
+      const downloadLink = document.createElement("a");
+      downloadLink.href = window.URL.createObjectURL(blob);
+      downloadLink.setAttribute("download", "@techielove.zip");
+
+      // Append the anchor to the body and trigger the click event
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+
+      // Clean up - remove the anchor element
+      document.body.removeChild(downloadLink);
+      setUploadStatus("");
+
+      // Return the response if needed
+      return response;
+    } catch (error) {
+      console.error("Error downloading zip file:", error);
+      throw error;
+    }
+  }
+
+  const handleUpload = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setUploadStatus("");
+    setIsloading(true);
+    const fileInput = document.getElementById("formFile") as HTMLInputElement;
+    const file = fileInput.files![0];
+    const formData = new FormData();
+    formData.append("file", file);
+
+    formData.append("password", password);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/upload");
+
+    xhr.upload.addEventListener("progress", (event) => {
+      if (event.lengthComputable) {
+        const progress = (event.loaded / event.total) * 100;
+        setUploadProgress(progress); // Update progress state
+      }
+    });
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        setIsloading(false);
+        setUploadStatus("Download Live Cookies");
+        setZipname(JSON.parse(xhr.responseText).zipname);
+        //console.log("200 res...",JSON.parse(xhr.responseText).zipname);
+      } else {
+        setIsloading(false);
+        setUploadStatus("No live Cookies");
+        //console.log("other res...",xhr.responseText);
+      }
+      // Reset upload progress after upload completion
+      setUploadProgress(0);
+    };
+
+    xhr.send(formData);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center w-screen h-screen">
+      <Card className="px-4 py-2 w-[320px] shadow-md">
+        <CardHeader className="bg-white">
+          <p className="font-normal text-center ">CHECK NETFLIX COOKIES</p>
+        </CardHeader>
+        <CardBody className="text-md font-normal ">
+          <Form onSubmit={handleUpload}>
+            <InputGroup className="flex flex-col my-3">
+              <label>Upload .zip or .rar</label>
+              <input
+                className="my-3 w-full text-ellipsis text-sm"
+                accept=".zip,.rar"
+                type="file"
+                id="formFile"
+                required
+              ></input>
+            </InputGroup>
+            <InputGroup className="flex flex-col my-3">
+              <Form.Check
+                type="switch"
+                label="Password protected"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setPasswordProtected(e.target.checked)
+                }
+              />
+            </InputGroup>
+            {passwordProtected && (
+              <InputGroup className="flex flex-col">
+                <input
+                  type="password"
+                  placeholder="Encryption password"
+                  className="px-2 py-1 rounded-md mt-2 border border-neutral-500 focus:border-blue-200 focus:border"
+                  value={password}
+                  onChange={handlePasswordChange}
+                ></input>
+              </InputGroup>
+            )}
+            {isloading ? (
+              <Button
+                type="submit"
+                disabled
+                className="my-2 bg-blue-600 w-full"
+              >
+                CHECKING
+              </Button>
+            ) : (
+              <Button type="submit" className="my-2 bg-blue-600 w-full">
+                UPLOAD
+              </Button>
+            )}
+
+            {/* Conditionally render progress bar */}
+            {uploadProgress > 0 && uploadProgress < 100 && (
+              <div className="mt-3 w-full">
+                <ProgressBar
+                  animated
+                  now={uploadProgress}
+                  label={`${uploadProgress.toFixed(0)}%`}
+                />
+              </div>
+            )}
+            {/* Show upload status */}
+            {uploadStatus &&
+              (uploadStatus === "Download Live Cookies" ? (
+                <p
+                  className="text-blue-700 text-center cursor-pointer"
+                  onClick={handleDownload}
+                >
+                  Download Live Cookies
+                </p>
+              ) : (
+                <p className="text-red-500 text-center">No Live Cookies</p>
+              ))}
+          </Form>
+          <CardFooter className="text-center bg-white mt-6">
+            <p className="text-sm pt-2">
+              Made ❤️ with by Flixchecker
+              <br></br>
+              <span className="text-[10px] mx-2 text-gray-400">
+                Copyright © 2024
+              </span>
+            </p>
+          </CardFooter>
+        </CardBody>
+      </Card>
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+// pages/index.js
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
+import { useRouter } from "next/router";
+import Link from 'next/link'
+
+export default function Home() {
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isloading, setIsloading] = useState<boolean>(false);
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+      setIsloading(true);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:8080/getpassword");
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          setIsloading(false);
+          if (password === JSON.parse(xhr.responseText).message) {
+            // Navigate to the dashboard page
+            window.location.href = "/dashboard";
+          } else {
+            setIsloading(false);
+
+            setError("Incorrect password. Please try again.");
+          }
+        } else {
+          setIsloading(false);
+          setError(JSON.parse(xhr.responseText).error);
+        }
+      };
+
+      xhr.send();
+    } catch (error) {
+      setIsloading(false);
+      console.log("ERROR ", error);
+      setError("Could not fetch password.");
+    }
+  }
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    setError(""); // Clear error message when password is changed
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center w-screen h-screen">
+      <Card className="px-1 py-2 w-[300px] shadow-md">
+        <CardHeader className="bg-white">
+          <p className="font-normal text-center ">PROTECTED SITE</p>
+        </CardHeader>
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
+            <InputGroup className="flex flex-col">
+              <label>Password</label>
+              <input
+                required
+                type="password"
+                placeholder="Enter a password"
+                className="px-2 py-1 rounded-md mt-2 border border-neutral-500 focus:border-blue-200 focus:border"
+                value={password}
+                onChange={handlePasswordChange}
+              ></input>
+            </InputGroup>
+            {error && <p className="text-red-500 mt-2">{error}</p>}
+            {isloading ? (
+              <Button
+                type="submit"
+                disabled
+                className="mt-3 bg-blue-300 w-full"
+              >
+                LOGING IN ...
+              </Button>
+            ) : (
+              <Button type="submit" className="mt-3 bg-blue-600 w-full">
+                LOGIN
+              </Button>
+            )}
+		<p className="text-center pt-3" >Get Password <Link className="text-blue-700 hover:font-bold" href="https://t.me/cookielogin">Here</Link></p>
+          </Form>
+        </CardBody>
+      </Card>
+    </main>
+  );
+}
