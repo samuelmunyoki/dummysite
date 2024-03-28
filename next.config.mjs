7784c219501208a19ab685cc00680493b21d63f9
Samuel Munyoki Muthembwa <samuelmuthembwa@yahoo.com>
2024-03-28 11:19:39 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,17 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
-            }
+            },
+          {
+            source: '/:path*',
+            headers: [
+             {
+               key: 'Content-Security-Policy',
+               value: "upgrade-insecure-requests"
+             }
+            ],
+          },
+     
         ]
     }
 };
